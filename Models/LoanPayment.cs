2644454af450b4ef58b5d7b9e8838073bfa35cf0
Adam Shively <adammshively89@gmail.com>
2022-09-22 09:01:37 -0400

    updated page titles and comments

--- Models/LoanPayment.cs
+++ Models/LoanPayment.cs
@@ -3,8 +3,6 @@
     public class LoanPayment
     {
         public int Month { get; set; }
-
-        //[DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
         public decimal PaymentAmount { get; set; }
         public decimal MonthlyPrincipal { get; set; }
         public decimal MonthlyInterest { get; set; }
